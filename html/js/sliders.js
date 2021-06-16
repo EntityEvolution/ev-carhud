1fcfcc4111bbabeb0c5ceca11d8d91ddf676192a
XiosBombay <OMGmsebastian@gmail.com>
2021-06-16 12:44:07 -0500

    Feat: Added sliders listeners

--- html/js/sliders.js
+++ html/js/sliders.js
@@ -10,107 +10,284 @@ const checkLeft = doc.getElementById('check-left')
 const checkWrench = doc.getElementById('check-wrench')
 const checkRight = doc.getElementById('check-right')
 
-const setSliders = () => {
-    
+const saveBreak = doc.getElementById('save-break')
+const brokeBreak = doc.getElementById('broke-break')
+const freezeBreak = doc.getElementById('freeze-break')
+
+let broke = false;
+let freeze = false;
+
+const setSliders = ()=> {
+  checkFuel.checked = getId("sliderFuel")
+  checkSpeed.checked = getId("sliderSpeed")
+  checkRpm.checked = getId("sliderRpm")
+  checkSeatbelt.checked = getId("sliderSeatbelt")
+  checkTacho.checked = getId("sliderTacho")
+  checkCogs.checked = getId("sliderCogs")
+  checkLeft.checked = getId("sliderLeft")
+  checkWrench.checked = getId("sliderWrench")
+  checkRight.checked = getId("sliderRight")
+
+  freezeBreak.checked = getId("freezeBreak")
+  brokeBreak.checked = getId("brokeBreak")
+  freeze = freezeBreak.checked
+  broke = brokeBreak.checked
+  
+  dragBreak()
+}
+
+// Set sliders to change onclick
+window.addEventListener('load', ()=> {
+  setSliders()
+  checkFuel.addEventListener('click', ()=> {
+    fuel = doc.getElementById('check-fuel').checked
+    const container = doc.getElementById('fuel-container')
+    if (fuel) {
+      container.style.display = 'flex'
+      saveId("sliderFuel", fuel)
+    } else {
+      container.style.display = 'none'
+      saveId("sliderFuel", fuel)
+    }
+  })
+
+  checkSpeed.addEventListener('click', ()=> {
+    speed = doc.getElementById('check-fuel').checked
+    const container = doc.getElementById('speed-container')
+    if (speed) {
+      container.style.display = 'flex'
+      saveId("sliderSpeed", speed)
+    } else {
+      container.style.display = 'none'
+      saveId("sliderSpeed", speed)
+    }
+  })
+
+  checkRpm.addEventListener('click', ()=> {
+    rpm = doc.getElementById('check-rpm').checked
+    const container = doc.getElementById('rpm-container')
+    if (rpm) {
+      container.style.display = 'flex'
+      saveId("sliderRpm", rpm)
+    } else {
+      container.style.display = 'none'
+      saveId("sliderRpm", rpm)
+    }
+  })
+
+  checkSeatbelt.addEventListener('click', ()=> {
+    seatbelt = doc.getElementById('check-seatbelt').checked
+    const element = doc.getElementById('seatbelt')
+    if (seatbelt) {
+      element.style.display = 'block'
+      saveId("sliderSeatbelt", seatbelt)
+    } else {
+      element.style.display = 'none'
+      saveId("sliderSeatbelt", seatbelt)
+    }
+  })
+
+  checkTacho.addEventListener('click', ()=> {
+    tacho = doc.getElementById('check-tacho').checked
+    const element = doc.getElementById('speedo')
+    if (tacho) {
+      element.style.display = 'block'
+      saveId("sliderTacho", tacho)
+    } else {
+      element.style.display = 'none'
+      saveId("sliderTacho", tacho)
+    }
+  })
+
+  checkCogs.addEventListener('click', ()=> {
+    cogs = doc.getElementById('check-tacho').checked
+    const element = doc.getElementById('gear')
+    if (cogs) {
+      element.style.display = 'block'
+      saveId("sliderCogs", cogs)
+    } else {
+      element.style.display = 'none'
+      saveId("sliderCogs", cogs)
+    }
+  })
+
+  checkLeft.addEventListener('click', ()=> {
+    left = doc.getElementById('check-left').checked
+    const element = doc.getElementById('left-arrow')
+    if (left) {
+      element.style.display = 'block'
+      saveId("sliderLeft", left)
+    } else {
+      element.style.display = 'none'
+      saveId("sliderLeft", left)
+    }
+  })
+
+  checkWrench.addEventListener('click', ()=> {
+    wrench = doc.getElementById('check-wrench').checked
+    const element = doc.getElementById('wrench')
+    if (wrench) {
+      element.style.display = 'block'
+      saveId("sliderWrench", wrench)
+    } else {
+      element.style.display = 'none'
+      saveId("sliderWrench", wrench)
+    }
+  })
+
+  checkRight.addEventListener('click', ()=> {
+    right = doc.getElementById('check-right').checked
+    const element = doc.getElementById('right')
+    if (right) {
+      element.style.display = 'block'
+      saveId("sliderRight", right)
+    } else {
+      element.style.display = 'none'
+      saveId("sliderRight", right)
+    }
+  })
+
+  // Save break and freeze
+  saveBreak.addEventListener('click', ()=> {
+    saveId("brokeBreak", broke);
+    saveId("brokeFreeze", freeze)
+  })
+
+  // Sliders for breaking drag
+  freezeBreak.addEventListener('click', ()=> {
+    freeze = document.getElementById("freeze-break").checked;
+    if (freeze) {
+      $("#wrapper").draggable({ disabled: true });
+      $("#icon-wrapper").draggable({ disabled: true });
+      $("#tab").draggable({ disabled: true });
+      if (broke) {
+        $("#gear").draggable({ disabled: true });
+        $("#right-arrow").draggable({ disabled: true });
+        $("#speedo").draggable({ disabled: true });
+        $("#seatbelt").draggable({ disabled: true });
+        $("#wrench").draggable({ disabled: true });
+        $("#left-arrow").draggable({ disabled: true });
+        $("#fuel-container").draggable({ disabled: true });
+        $("#rpm-container").draggable({ disabled: true });
+        $("#speed-container").draggable({ disabled: true });
+      }
+    } else {
+      $("#wrapper").draggable({ disabled: false });
+      $("#icon-wrapper").draggable({ disabled: false });
+      $("#tab").draggable({ disabled: false });
+      if (broke) {
+        $("#gear").draggable({ disabled: false });
+        $("#right-arrow").draggable({ disabled: false });
+        $("#speedo").draggable({ disabled: false });
+        $("#seatbelt").draggable({ disabled: false });
+        $("#wrench").draggable({ disabled: false });
+        $("#left-arrow").draggable({ disabled: false });
+        $("#fuel-container").draggable({ disabled: false });
+        $("#rpm-container").draggable({ disabled: false });
+        $("#speed-container").draggable({ disabled: false });
+      }
+    }
+  })
+
+  brokeBreak.addEventListener('click', ()=> {
+    broke = document.getElementById("broke-break").checked;
+    if (broke && !freeze) {
+      $("#gear").draggable({ disabled: false });
+      $("#right-arrow").draggable({ disabled: false });
+      $("#speedo").draggable({ disabled: false });
+      $("#seatbelt").draggable({ disabled: false });
+      $("#wrench").draggable({ disabled: false });
+      $("#left-arrow").draggable({ disabled: false });
+      $("#fuel-container").draggable({ disabled: false });
+      $("#rpm-container").draggable({ disabled: false });
+      $("#speed-container").draggable({ disabled: false });
+    } else {
+      $("#gear").draggable({ disabled: true });
+      $("#right-arrow").draggable({ disabled: true });
+      $("#speedo").draggable({ disabled: true });
+      $("#seatbelt").draggable({ disabled: true });
+      $("#wrench").draggable({ disabled: true });
+      $("#left-arrow").draggable({ disabled: true });
+      $("#fuel-container").draggable({ disabled: true });
+      $("#rpm-container").draggable({ disabled: true });
+      $("#speed-container").draggable({ disabled: true });
+    }
+  })
+
+  // Tab listeners
+  doc.getElementById('restore').addEventListener('click', function() {
+    $("#tab").animate({ top: "5%", left: "50%" }); 
+  })
+
+  doc.getElementById('close').addEventListener('click', function() {
+    $.post(`https://${GetParentResourceName()}/close`);
+  })
+})
+
+let dragBreak = ()=> {
+  if (freeze) {
+      $("#wrapper").draggable({ disabled: true });
+      $("#icon-wrapper").draggable({ disabled: true });
+      $("#tab").draggable({ disabled: true });
+      if (broke) {
+        $("#gear").draggable({ disabled: true });
+        $("#right-arrow").draggable({ disabled: true });
+        $("#speedo").draggable({ disabled: true });
+        $("#seatbelt").draggable({ disabled: true });
+        $("#wrench").draggable({ disabled: true });
+        $("#left-arrow").draggable({ disabled: true });
+        $("#fuel-container").draggable({ disabled: true });
+        $("#rpm-container").draggable({ disabled: true });
+        $("#speed-container").draggable({ disabled: true });
+      }
+    } else {
+      $("#wrapper").draggable({ disabled: false });
+      $("#icon-wrapper").draggable({ disabled: false });
+      $("#tab").draggable({ disabled: false });
+      if (broke) {
+        $("#gear").draggable({ disabled: false });
+        $("#right-arrow").draggable({ disabled: false });
+        $("#speedo").draggable({ disabled: false });
+        $("#seatbelt").draggable({ disabled: false });
+        $("#wrench").draggable({ disabled: false });
+        $("#left-arrow").draggable({ disabled: false });
+        $("#fuel-container").draggable({ disabled: false });
+        $("#rpm-container").draggable({ disabled: false });
+        $("#speed-container").draggable({ disabled: false });
+      }
+    }
+
+  if (broke && !freeze) {
+    $("#gear").draggable({ disabled: false });
+    $("#right-arrow").draggable({ disabled: false });
+    $("#speedo").draggable({ disabled: false });
+    $("#seatbelt").draggable({ disabled: false });
+    $("#wrench").draggable({ disabled: false });
+    $("#left-arrow").draggable({ disabled: false });
+    $("#fuel-container").draggable({ disabled: false });
+    $("#rpm-container").draggable({ disabled: false });
+    $("#speed-container").draggable({ disabled: false });
+  } else {
+    $("#gear").draggable({ disabled: true });
+    $("#right-arrow").draggable({ disabled: true });
+    $("#speedo").draggable({ disabled: true });
+    $("#seatbelt").draggable({ disabled: true });
+    $("#wrench").draggable({ disabled: true });
+    $("#left-arrow").draggable({ disabled: true });
+    $("#fuel-container").draggable({ disabled: true });
+    $("#rpm-container").draggable({ disabled: true });
+    $("#speed-container").draggable({ disabled: true });
+  }
 }
 
 
 
-$("#check-fuel").click( () => { 
-  let fuel = document.getElementById("check-fuel").checked;
-  if (fuel) {
-    document.getElementById("fuel-container").style.display = "flex"
-    localStorage.setItem("sliderFuel", fuel);
-  } else {
-    document.getElementById("fuel-container").style.display = "none"
-    localStorage.setItem("sliderFuel", fuel);
-  }
-});
+// Short localstorage
+function saveId(item, check) {
+  localStorage.setItem(item, check);
+}
 
-$("#check-speed").click( () => { 
-  let speed = document.getElementById("check-speed").checked;
-  if (speed) {
-    document.getElementById("speed-container").style.display = "flex"
-    localStorage.setItem("sliderSpeed", speed);
-  } else {
-    document.getElementById("speed-container").style.display = "none"
-    localStorage.setItem("sliderSpeed", speed);
-  }
-});
-
-$("#check-rpm").click( () => { 
-  let rpm = document.getElementById("check-rpm").checked;
-  if (rpm) {
-    document.getElementById("rpm-container").style.display = "flex"
-    localStorage.setItem("sliderRpm", rpm);
-  } else {
-    document.getElementById("rpm-container").style.display = "none"
-    localStorage.setItem("sliderRpm", rpm);
-  }
-});
-
-$("#check-seatbelt").click( () => { 
-  let seatbelt = document.getElementById("check-seatbelt").checked;
-  if (seatbelt) {
-    document.getElementById("seatbelt").style.display = "block"
-    localStorage.setItem("sliderSeatbelt", seatbelt);
-  } else {
-    document.getElementById("seatbelt").style.display = "none"
-    localStorage.setItem("sliderSeatbelt", seatbelt);
-  }
-});
-
-$("#check-tacho").click( () => { 
-  let tacho = document.getElementById("check-tacho").checked;
-  if (tacho) {
-    document.getElementById("speedo").style.display = "block"
-    localStorage.setItem("sliderTacho", tacho);
-  } else {
-    document.getElementById("speedo").style.display = "none"
-    localStorage.setItem("sliderTacho", tacho);
-  }
-});
-
-$("#check-cogs").click( () => { 
-  let cogs = document.getElementById("check-cogs").checked;
-  if (cogs) {
-    document.getElementById("gear").style.display = "block"
-    localStorage.setItem("sliderCogs", cogs);
-  } else {
-    document.getElementById("gear").style.display = "none"
-    localStorage.setItem("sliderCogs", cogs);
-  }
-});
-
-$("#check-left").click( () => { 
-  let left = document.getElementById("check-left").checked;
-  if (left) {
-    document.getElementById("left-arrow").style.display = "block"
-    localStorage.setItem("sliderLeft", left);
-  } else {
-    document.getElementById("left-arrow").style.display = "none"
-    localStorage.setItem("sliderLeft", left);
-  }
-});
-
-$("#check-wrench").click( () => { 
-  let wrench = document.getElementById("check-wrench").checked;
-  if (wrench) {
-    document.getElementById("wrench").style.display = "block"
-    localStorage.setItem("sliderWrench", wrench);
-  } else {
-    document.getElementById("wrench").style.display = "none"
-    localStorage.setItem("sliderWrench", wrench);
-  }
-});
-
-$("#check-right").click( () => { 
-  let right = document.getElementById("check-right").checked;
-  if (right) {
-    document.getElementById("right-arrow").style.display = "block"
-    localStorage.setItem("sliderRight", right);
-  } else {
-    document.getElementById("right-arrow").style.display = "none"
-    localStorage.setItem("sliderRight", right);
-  }
-});
+function getId(item) {
+  let storage = localStorage.getItem(JSON.parse(item));
+  return storage
+}
